aab5eeb20d0db951547872203d11e8e0bea0a517
Andreas Nilsson <andreas.nilsson@hkr.se>
2023-03-17 18:33:29 +0100

    Solved and added a second line in the textfile

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,26 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws FileNotFoundException {
         // Läs in en fil med rövarspråk, översätt det till klartext och skriv ut det på skärmen
 
-
+        Scanner fileIn = new Scanner(new File("translated.txt"));
+        while (fileIn.hasNextLine()) {
+            String line = fileIn.nextLine();
+            for (int i=0; i < line.length(); i++) {
+                char c = line.charAt(i);
+                switch (c) {
+                    case 'a', 'e', 'i', 'o', 'u', 'y', 'å', 'ä', 'ö' -> System.out.print(c);
+                    case ' ', '.' -> System.out.print(c);
+                    default -> {
+                        System.out.print(c);
+                        i += 2;
+                    }
+                }
+            }
+            System.out.println();
+        }
     }
 }
